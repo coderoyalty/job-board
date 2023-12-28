22161229753b6b8ccb508d943ee5a9bb0691ce27
Akanni Emmanuel <akannie33@gmail.com>
2023-12-28 08:48:13 +0100

    add dotenv, cookie-parse and jsonwebtoken package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,15 @@
 import "./app";
 import "module-alias/register";
+import dotenv from "dotenv";
 import "@/controllers";
 import App from "@/app";
 import connectDB from "./utils/database";
-import User, { Role } from "./models/user";
+dotenv.config();
 
 const app = App.getInstance();
 //.. your database connections and other configurations can happen here!
 
 // run the application
-// const server = app.run();
-
 connectDB().catch(() => {
 	process.exit(0);
 });
