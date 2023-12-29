df38b0aa8b52feef83d566718467788520b5b652
Akanni Emmanuel <akannie33@gmail.com>
2023-12-29 14:57:34 +0100

    feat: add endpoint to submit user role information - validators for candidate and employer data - middleware to intercept unauthorized action - implemented the role creation logic in user service class

--- backend/src/errors/index.ts
+++ backend/src/errors/index.ts
@@ -1,4 +1,6 @@
 import CustomAPIError from "./custom.api";
 import BadRequestError from "./bad.request";
+import UnauthorizedError from "./unauthorized";
+import NotFoundError from "./not-found";
 
-export { CustomAPIError, BadRequestError };
+export { CustomAPIError, BadRequestError, UnauthorizedError, NotFoundError };
--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,8 +1,10 @@
-import { CustomAPIError } from "@/errors";
+import { CustomAPIError, NotFoundError } from "@/errors";
 import Candidate from "@/models/candidate";
 import Employer from "@/models/employer";
 import User, { Role } from "@/models/user";
+import { CandidateValidator, EmployerValidator } from "@/validators/user";
 import { StatusCodes } from "http-status-codes";
+import { z } from "zod";
 
 class UserService {
 	static async getUserAndRole(id: string) {
@@ -28,6 +30,33 @@ class UserService {
 		}
 
 		return { user, data: roleValue };
+	}
+
+	static async createUserRole(
+		userId: string,
+		data:
+			| z.infer<typeof CandidateValidator>
+			| z.infer<typeof EmployerValidator>,
+	) {
+		const user = await User.findById(userId);
+
+		if (!user) {
+			throw new NotFoundError("Couldn't find any user with the provided id");
+		}
+
+		let roleData = null;
+		if (user.role === Role.CANDIDATE) {
+			roleData = await Candidate.create({ ...data, user: user.id });
+		} else {
+			roleData = await Employer.create({ ...data, user: user.id });
+		}
+
+		await roleData.save();
+
+		return {
+			user,
+			roleData,
+		};
 	}
 }
 
