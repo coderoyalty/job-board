6241e152e816d2c2fa7698bc407a3219eb6f14ab
Akanni Emmanuel <akannie33@gmail.com>
2023-12-30 15:43:36 +0100

    frontend: editor prototype

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import ReactQuillWrapper from "./ReactQuillWrapper";
+import {
+  Container,
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Center,
+} from "@chakra-ui/react";
 
+// TODO:
 export default function () {
   const [text, setText] = React.useState("");
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <>
-      <h1 className="text-3xl font-semibold py-4 text-center">
-        Quill React Editor
-      </h1>
-      <ReactQuillWrapper {...{ text, setText }} />
+      <Container>
+        <h1 className="text-3xl font-semibold py-4 text-center">
+          Quill React Editor
+        </h1>
+        <ReactQuillWrapper {...{ text, setText }} className="bg-slate-400" />
+        <div className="m-8">
+          <Center>
+            <Button onClick={onOpen}>Submit</Button>
+          </Center>
+        </div>
+      </Container>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div dangerouslySetInnerHTML={{ __html: text }}></div>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
