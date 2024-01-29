f020c1d436fd104029693c41fef8395fc8169c82
Akanni Emmanuel <akannie33@gmail.com>
2024-01-29 22:46:28 +0100

    refactor: update TopBar user email display and EmployerInformationForm submission
    - In the TopBar component, modified the logic to display the user email without unnecessary console.log statements.
    - In the EmployerInformationForm component, implemented form submission using the useFormik hook and axios for API requests. Also added loading state for the submit button.

--- frontend/src/components/form/EmployerInformationForm.jsx
+++ frontend/src/components/form/EmployerInformationForm.jsx
@@ -1,94 +1,106 @@
 import {
   Stack,
-  HStack,
   Heading,
   Button,
   Box,
   FormControl,
   FormLabel,
-  FormHelperText,
   Input,
   Textarea,
-  IconButton,
 } from "@chakra-ui/react";
-import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+import { useFormik } from "formik";
 import React from "react";
+import axios from "../../api/axios";
+import useAuth from "../../hooks/useAuth";
+import { AxiosError } from "axios";
 
-const EmployerInformationForm = () => {
-  const [showAdditionalFields, setShow] = React.useState(false);
-  const FormField1 = () => {
-    return (
-      <>
-        <FormControl isRequired>
-          <FormLabel>Contact Name</FormLabel>
-          <Input type="text" name="contactName" id="contactName" />
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel htmlFor="companyName">Company Name</FormLabel>
-          <Input type="text" name="companyName" id="companyName" />
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel htmlFor="companyWebsite">Company Website</FormLabel>
-          <Input type="url" name="companyWebsite" id="companyWebsite" />
-          <FormHelperText>
-            Use the company LinkedIn URL if there&apas;s no website.
-          </FormHelperText>
-        </FormControl>
-      </>
-    );
+// eslint-disable-next-line react/prop-types
+const EmployerInformationForm = ({ onClose }) => {
+  const { userData, login } = useAuth();
+  const [isLoading, setLoading] = React.useState(false);
+
+  const handleSubmit = async (values) => {
+    setLoading(true);
+
+    const userId = userData.user.id;
+
+    try {
+      await axios.post(`/users/${userId}/role`, { ...values });
+      onClose();
+      login(userData.data.data);
+    } catch (err) {
+      if (err instanceof AxiosError);
+    }
+
+    formik.resetForm({
+      contactName: "",
+      companyName: "",
+      companyLocation: "",
+      companyDescription: "",
+    });
+    setLoading(false);
   };
 
-  const FormField2 = () => {
-    return (
-      <>
-        <FormControl isRequired>
-          <FormLabel htmlFor="companyName">Company Location</FormLabel>
-          <Input type="text" name="companyLocation" id="companyLocation" />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="companyDesc">Company Description</FormLabel>
-          <Textarea name="companyDesc" id="companyDesc" />
-        </FormControl>
-      </>
-    );
-  };
+  const formik = useFormik({
+    initialValues: {
+      contactName: "",
+      companyName: "",
+      companyLocation: "",
+      companyDescription: "",
+    },
+    onSubmit: handleSubmit,
+  });
 
   return (
     <Stack direction="column" spacing={6} className="p-4">
       <Heading textAlign="center">Employer Information</Heading>
-      <Box as="form">
+      <Box as="form" onSubmit={formik.handleSubmit}>
         <Stack direction={"column"} spacing={4}>
-          {!showAdditionalFields ? <FormField1 /> : <FormField2 />}
+          <FormControl isRequired>
+            <FormLabel>Contact Name</FormLabel>
+            <Input
+              type="text"
+              name="contactName"
+              id="contactName"
+              value={formik.values.contactName}
+              onChange={formik.handleChange}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="companyName">Company Name</FormLabel>
+            <Input
+              type="text"
+              name="companyName"
+              id="companyName"
+              value={formik.values.companyName}
+              onChange={formik.handleChange}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="companyName">Company Location</FormLabel>
+            <Input
+              type="text"
+              name="companyLocation"
+              id="companyLocation"
+              value={formik.values.companyLocation}
+              onChange={formik.handleChange}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="companyDesc">Company Description</FormLabel>
+            <Textarea
+              name="companyDescription"
+              id="companyDescription"
+              value={formik.values.companyDescription}
+              onChange={formik.handleChange}
+            />
+          </FormControl>
 
-          {/* <ButtonGroup> */}
-          <HStack>
-            <IconButton
-              isDisabled={!showAdditionalFields}
-              icon={<ArrowLeftIcon />}
-              id="leftNavBtn"
-              w={"50%"}
-              variant={"outline"}
-              colorScheme="telegram"
-              onClick={() => {
-                setShow(false);
-              }}
-            />
-            <IconButton
-              isDisabled={showAdditionalFields}
-              icon={<ArrowRightIcon />}
-              id="rightNavBtn"
-              w={"50%"}
-              variant={"outline"}
-              colorScheme="telegram"
-              onClick={() => {
-                setShow(true);
-              }}
-            />
-          </HStack>
           <Button
             colorScheme="twitter"
             type="submit"
-            isDisabled={!showAdditionalFields}
+            isLoading={isLoading}
+            isDisabled={isLoading}
           >
             Submit
           </Button>
