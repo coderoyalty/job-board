a951af738c08fae77b4e4843d4cc894038a2f982
Akanni Emmanuel <akannie33@gmail.com>
2024-01-30 15:43:12 +0100

    chore(frontend): add TimeAgo package from literal time interpretation

--- frontend/src/components/job/JobCard.jsx
+++ frontend/src/components/job/JobCard.jsx
@@ -1,59 +1,7 @@
 /* eslint-disable react/prop-types */
-const data = {
-  jobTitle: "Office Administrator",
-  created: new Date(),
-  salary: "70,000.00",
-  location: "Ojodu, Berger",
-  summary: `Life Edge Nigeria is hiring for the role of Office
-                Administrator. The ideal candidate will be a young, intelligent,
-                creative person with previous clerical or administrative
-                experience who lives in...`,
-  company: "LIFE EDGE NIGERIA LTD",
-  work_mode: "remote",
-  applicants: 400,
-  deadline: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-};
-
 import { Badge, Stack, StackItem } from "@chakra-ui/react";
+import TimeAgo from "timeago-react";
 import { Link } from "react-router-dom";
-
-/**
- *
- * @param {Date} start
- * @param {Date} end
- */
-function findTimeLeft(start, end) {
-  const diff = Math.abs(end - start);
-  const inMinutes = 1000 * 60;
-  const inHours = inMinutes * 60;
-  const inDays = inHours * 24;
-  const inWeeks = inDays * 7;
-  const inMonths = inWeeks * 4;
-  let value = "";
-  if (diff < inHours) {
-    const time = Math.floor(diff / inMinutes);
-    if (time == 1) value = `A minute`;
-    else value = `${time} minutes`;
-  } else if (diff < inDays) {
-    const time = Math.floor(diff / inHours);
-    if (time == 1) value = `An hour`;
-    else value = `${time} hours`;
-  } else if (diff < inWeeks) {
-    const time = Math.floor(diff / inDays);
-    if (time == 1) value = `A day`;
-    else value = `${time} days`;
-  } else if (diff < inMonths) {
-    const time = Math.floor(diff / inWeeks);
-    if (time == 1) value = `A week`;
-    else value = `${time} weeks`;
-  } else {
-    const time = Math.floor(diff / inMonths);
-    if (time == 1) value = `A month`;
-    else value = `${time} months`;
-  }
-
-  return `${value} to go`;
-}
 
 const JobCard = ({ data }) => {
   return (
@@ -61,7 +9,6 @@ const JobCard = ({ data }) => {
       <div className="transition-all delay-75 mx-2 w-[324px] sm:w-[424px] md:max-w-[482px] bg-[#E2C799] shadow-md rounded overflow-hidden hover:backdrop:blur-lg hover:bg-[rgba(255,255,255,0.45)]">
         <Link to={`/job/${data.id}`}>
           <Stack direction={"column"} spacing={1} className="p-2 md:p-4">
-            {/* Job Info */}
             <div className="flex justify-between">
               <div className="font-bold">{data.title}</div>
               <div>
@@ -72,7 +19,6 @@ const JobCard = ({ data }) => {
                 </Badge>
               </div>
             </div>
-            {/* Salary */}
             <div className="flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -124,7 +70,8 @@ const JobCard = ({ data }) => {
             <Stack direction={"row"}>
               <StackItem>
                 <Badge colorScheme="whatsapp">
-                  <span className="text-sm">{data.work_mode}</span>
+                  {/* TODO: work_mode goes here e.g Remote, Onsite, Hybrid */}
+                  <span className="text-sm"></span>
                 </Badge>
               </StackItem>
               <StackItem>
@@ -135,10 +82,7 @@ const JobCard = ({ data }) => {
               <StackItem>
                 <Badge className="twitter animate-bounce">
                   <span className="text-sm">
-                    {findTimeLeft(
-                      new Date(data.createdAt),
-                      new Date(data.deadline)
-                    )}
+                    <TimeAgo datetime={data.deadline} />
                   </span>
                 </Badge>
               </StackItem>
